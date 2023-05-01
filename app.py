d362a4184e2598169f1a737104755a015dd8395b
Vishal21121 <89929777+Vishal21121@users.noreply.github.com>
2023-05-01 04:46:57 +0000

    added html and css

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # there is module flask and class is Flask
-from flask import Flask
+from flask import Flask, render_template
 
 # creating an instance of the Flask class
 app = Flask(__name__)
@@ -7,7 +7,7 @@ app = Flask(__name__)
 # here we are using decorator basically we are passing the below function as argument to app.route funnction
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template('home.html')
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0",debug=True)
